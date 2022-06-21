75baafb0eb1aee4339173939f15bf86e13d9832d
Lynette7 <lynettewanjiru7@gmail.com>
2022-06-21 19:15:58 +0000

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -10,7 +10,7 @@ class Square:
     """
 
     def __init__(self, size=0):
-        """Initializes the data."""
+        """Initializes the square."""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
