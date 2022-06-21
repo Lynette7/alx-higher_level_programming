590e28780cd6a146efb5cca723501faf47a109fc
Lynette7 <lynettewanjiru7@gmail.com>
2022-06-21 19:07:05 +0000

    Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,12 @@ class Square:
     """
 
     def __init__(self, size=0):
-        """Initializes the square."""
+        """Initializes the square.
+	Args:
+        size (int): size of a side of the square
+        Returns:
+        None
+	"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
